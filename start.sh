df30415fcf2e564c41f4d0be926888ed650574e6
赵睿 <zhaorui27@sfmail.sf-express.com>
2022-04-27 10:35:49 +0800

    feat(start.sh): linux 环境启动脚本

---
+++ start.sh
@@ -0,0 +1,6 @@
+# 创建目录
+mkdir -p browsers/chromium-1000/chrome-linux
+# 浏览器创建软连接
+ln -s /usr/bin/google-chrome browsers/chromium-1000/chrome-linux/chrome
+# 运行测试
+PLAYWRIGHT_BROWSERS_PATH=browsers npx playwright test
