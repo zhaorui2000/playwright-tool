13ca55a848667ecf7bb91d4622bf70390f94f986
赵睿 <zhaorui27@sfmail.sf-express.com>
2022-04-27 10:47:55 +0800

    feat: 更新测试文件

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,18 +10,7 @@ test.describe("playwright测试", () => {
   test("三分之一概率成功", () => {
     expect(Math.floor(Math.random() * 3)).toBe(1);
   });
-  test("访问百度", async ({}) => {
-    const browser = await chromium.launch({
-      headless: false,
-      args: ["--no-sandbox", "--no-zygote"],
-    });
-    const page = await browser.newPage();
-    // Go to https://www.baidu.com/
-    await page.goto("https://www.baidu.com/");
-    // Click text=换一换
-    await page.click("text=换一换");
-    expect(await page.$("text=换一换")).not.toBeNull();
-  });
+  
   test("肯定会失败", () => {
     expect(false).toBeTruthy();
   });
