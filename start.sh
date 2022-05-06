590fd0c03c5911b5a75dd0e123eed0760f829f07
赵睿 <zhaorui27@sfmail.sf-express.com>
2022-05-06 12:13:46 +0800

    feat: start.sh 运行前装包 .npmrc 设置内源地址

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 mkdir -p browsers/chromium-1000/chrome-linux
 # 浏览器创建软连接
 ln -s /usr/bin/google-chrome browsers/chromium-1000/chrome-linux/chrome
+# 装包
+npm install
 # 运行测试
 PLAYWRIGHT_BROWSERS_PATH=browsers npx playwright test
