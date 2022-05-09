6cae8235ce49c7061fd0fdc4255bcd275c1d02a8
赵睿 <zhaorui27@sfmail.sf-express.com>
2022-05-09 13:54:55 +0800

    test: 测试 tags 是不是好用了

--- start.sh
+++ start.sh
@@ -5,14 +5,13 @@ ln -s /usr/bin/google-chrome browsers/chromium-1000/chrome-linux/chrome
 # 装包
 npm install
 # 运行
-# --- 获取参数
+# --- jenkins中参数
+project=${project} #项目名称
+tags=${tags}       #自定义标签
+# --- DONE jenkins中参数
+# --- 获取命令行参数
 while [ $# -gt 0 ]; do
   case "$1" in
-  -a | --args)
-    args=$2
-    shift
-    break
-    ;;
   --id)
     id=$2
     shift
@@ -20,8 +19,27 @@ while [ $# -gt 0 ]; do
     ;;
   esac
 done
-# --- DONE 获取参数
+# --- DONE 获取命令行参数
+# --- 处理参数
+transformTags() {
+  OLD_IFS="$IFS"
+  IFS=" "
+  array=($*)
+  IFS="$OLD_IFS"
+  str=""
+  for ((i = 0; i < ${#array[@]}; i++)); do
+    if (($i == 0)); then
+      str=${array[0]}
+    else
+      str=$str"|"${array[$i]}
+    fi
+  done
+  tags=$str
+  return 0
+}
+transformTags $tags
+echo $tags
+# --- DONE 处理参数
 # --- 运行测试
-echo ${tags}
-PLAYWRIGHT_BROWSERS_PATH=browsers JENKINS_ID=$id npx playwright test -g ${project}
+PLAYWRIGHT_BROWSERS_PATH=browsers JENKINS_ID=$id npx playwright test ${project} -g ${tags}
 # --- DONE 运行测试
