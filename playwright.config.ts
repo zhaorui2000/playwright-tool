ec708c4e54105c896b35918cb545cbd3cea4b11a
赵睿 <zhaorui27@sfmail.sf-express.com>
2022-05-09 11:14:00 +0800

    feat: 支持传入参数

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import {PlaywrightTestConfig} from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   timeout: 5 * 60 * 1000,
-  reporter: [['html', {open: 'never', outputFolder: "/home/sftcwl/odp_ohs8139/webroot/fe"}]],
+  reporter: [['html', {open: 'never', outputFolder: `/home/sftcwl/odp_ohs8139/webroot/fe/${process.env.JENKINS_ID}`, }]],
   use: {
     locale: "zh-CN",
     actionTimeout: 10 * 1000,
--- start.sh
+++ start.sh
@@ -4,5 +4,23 @@ mkdir -p browsers/chromium-1000/chrome-linux
 ln -s /usr/bin/google-chrome browsers/chromium-1000/chrome-linux/chrome
 # 装包
 npm install
-# 运行测试
-PLAYWRIGHT_BROWSERS_PATH=browsers npx playwright test
+# 运行
+# --- 获取参数
+while [ $# -gt 0 ]; do
+  case "$1" in
+  -a | --args)
+    args=$2
+    shift
+    break
+    ;;
+  --id)
+    id=$2
+    shift
+    break
+    ;;
+  esac
+done
+# --- DONE 获取参数
+# --- 运行测试
+PLAYWRIGHT_BROWSERS_PATH=browsers JENKINS_ID=$id npx playwright test $args
+# --- DONE 运行测试
