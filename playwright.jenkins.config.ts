e90bcd910d83ab5df356f45a91228c3c8972b93f
赵睿 <zhaorui27@sfmail.sf-express.com>
2022-05-11 16:47:53 +0800

    feat(playwright.jenkins.config): 重试次数改为3次

--- playwright.jenkins.config.ts
+++ playwright.jenkins.config.ts
@@ -2,7 +2,8 @@ import {PlaywrightTestConfig} from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   timeout: 60 * 1000,
-  reporter: [['html', {open: 'never', outputFolder: `/home/sftcwl/odp_ohs8139/webroot/fe/${process.env.JENKINS_ID}`, }]],
+  retries: 3,
+  reporter: [['html', {open: 'never', outputFolder: `/home/sftcwl/odp_ohs8139/webroot/fe/${process.env.JENKINS_ID}`,}]],
   use: {
     locale: "zh-CN",
     screenshot: "on",
